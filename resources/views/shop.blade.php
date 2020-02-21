1b811731260071de307e29522232ec2aa781d5e2
karitelena <capozzer@gmail.com>
2020-02-21 12:28:21 -0300

    update

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,37 +2,57 @@
 
 @section ('content')
 <div>
-  <main>
-    <div class="shopList">
-        <div class="card-group">
-            <ul class="d-flex justify-content-center" style="list-style-type: none;">
-                <li class="productItem flex-fill bd-highlight">
-                    <a href="#">
-                        <div class="p-0 m-0">
-                            <div class="card" style="max-width: 20rem;">
-                                <img src="{{ asset('/img/20201/IMG_5675.jpg') }}" class="card-img-top" alt="...">
-                                    <div class="card-body text-center">
-                                        <p class="card-title">SQUARE BLACK</p>
-                                        <strong class="card-text">R$ 125</strong>    
-                            </div>
+    <main class="shopList container-fluid">
+        <div class="card-group col-md">
+            <div class="productItem">
+                <a href="#">
+                    <div class="p-0 m-0">
+                        <div class="card" style="max-width: 20rem;">
+                            <img src="{{ asset('/img/20201/IMG_5723.jpg') }}" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <p class="card-title">SLIP BLACK</p>
+                                    <strong class="card-text">R$ 59</strong>    
                         </div>
-                    </a>
-                </li>
-                <li class="productItem">
-                    <a href="#">
-                        <div class="p-0 m-0">
-                            <div class="card" style="max-width: 20rem;">
-                                <img src="{{ asset('/img/20201/IMG_5723.jpg') }}" class="card-img-top" alt="...">
-                                    <div class="card-body text-center">
-                                        <p class="card-title">SLIP BLACK</p>
-                                        <strong class="card-text">R$ 59</strong>    
-                            </div>
+                    </div>
+                </a>
+            </div>
+            <div class="productItem">
+                <a href="#">
+                    <div class="p-0 m-0">
+                        <div class="card" style="max-width: 20rem;">
+                            <img src="{{ asset('/img/20201/IMG_5723.jpg') }}" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <p class="card-title">SLIP BLACK</p>
+                                    <strong class="card-text">R$ 59</strong>    
                         </div>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-  </main>
+                    </div>
+                </a>
+            </div>
+            <div class="productItem">
+                <a href="#">
+                    <div class="p-0 m-0">
+                        <div class="card" style="max-width: 20rem;">
+                            <img src="{{ asset('/img/20201/IMG_5723.jpg') }}" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <p class="card-title">SLIP BLACK</p>
+                                    <strong class="card-text">R$ 59</strong>    
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="productItem">
+                <a href="#">
+                    <div class="p-0 m-0">
+                        <div class="card" style="max-width: 20rem;">
+                            <img src="{{ asset('/img/20201/IMG_5723.jpg') }}" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <p class="card-title">SLIP BLACK</p>
+                                    <strong class="card-text">R$ 59</strong>    
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>       
+    </main>
 </div>
 @endsection 
